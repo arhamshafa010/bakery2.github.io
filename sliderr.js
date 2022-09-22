a1e113d1dce4a89380a6074ea5c4d2a8f929c0d8
Arhamshafa <105345188+arhamshafa010@users.noreply.github.com>
2022-09-22 15:54:43 +0500

    2nd time checking

---
+++ sliderr.js
@@ -0,0 +1,34 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+
+  });
+
+  $('.logos').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    // dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    // dots: true,
+
+  });
+  $('.topslider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+
+  });
